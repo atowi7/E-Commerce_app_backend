d26ab46a542abc18cd57b89b8484ee01551e19ba
Abdulrahman Kamal Alatowi <36438434+atowi7@users.noreply.github.com>
2024-05-22 20:48:50 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=u101772973_ecommerceapp;';
-$username='u101772973_atowi7_app0';
-$password='123Fdhkhj';
+$dsn = 'mysql:host=localhost;dbname=#;';
+$username='#';
+$password='#';
 $options=array(
     PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES UTF8'
 );
